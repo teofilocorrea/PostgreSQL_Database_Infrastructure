c00461d9177e91dd035544f6fcec004a41f7a639
Teófilo Correa Rojas <teofilocorrea@gmail.com>
2026-05-02 00:19:33 -0400

    feat: create stg, bronze, silver, gold and governance schemas

---
+++ sql/00_setup/02_create_schemas_basic.sql
@@ -0,0 +1,7 @@
+----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS stg;
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+CREATE SCHEMA IF NOT EXISTS governance;
+----------------------------------------------------
